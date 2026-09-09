64d3222afdfc6345d2371f5bdb0a9f439471490f
CineVerse <safarovabduraxmin@gmail.com>
2026-09-09 12:50:21 +0500

    Fix local allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-am75e-ldco2)f48v56m9!itpd&%y09!-j_z32*4m&uidrw9e68
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["cineverse-ksqg.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "cineverse-ksqg.onrender.com"]
 
 
 # Application definition
